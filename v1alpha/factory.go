c61f3f5f11e474999f7106c1fcc2a23ca01f2a21
wenooij <142677213+wenooij@users.noreply.github.com>
2023-08-24 23:38:09 -0400

    v1alpha: Factory load from map lookup

--- v1alpha/factory.go
+++ v1alpha/factory.go
@@ -10,30 +10,28 @@ import (
 
 type Factory struct{}
 
-func (*Factory) NewRunner(n nuggit.Node) (runtime.Runner, error) {
-	var op runtime.Runner
-	switch n.Op {
-	case "Chromedp":
-		op = &Chromedp{}
-	case "Const":
-		op = &Const{}
-	case "HTTP":
-		op = &HTTP{}
-	case "Sink":
-		op = &Sink{}
-	case "Source":
-		op = &Source{}
-	case "String":
-		op = &String{}
-	case "Var":
-		op = &Var{}
-	default:
-		return nil, fmt.Errorf("NewRunner: Runner is not defined for Op: %q: %q", n.Key, n.Op)
+func (Factory) NewRunner(n nuggit.Node) (runtime.Runner, error) {
+	opFn, ok := factoryMapping[n.Op]
+	if !ok {
+		return nil, fmt.Errorf("v1alpha.Factory.NewRunner: Node %q: Runner is not defined for Op: %q", n.Key, n.Op)
 	}
+	op := opFn()
 	if len(n.Data) > 0 {
 		if err := json.Unmarshal(n.Data, op); err != nil {
 			return nil, err
 		}
 	}
 	return op, nil
+}
+
+var factoryMapping = map[string]func() runtime.Runner{
+	"Chromedp": func() runtime.Runner { return &Chromedp{} },
+	"Const":    func() runtime.Runner { return &Const{} },
+	"HTML":     func() runtime.Runner { return &HTML{} },
+	"HTTP":     func() runtime.Runner { return &HTTP{} },
+	"Selector": func() runtime.Runner { return &Selector{} },
+	"Sink":     func() runtime.Runner { return &Sink{} },
+	"Source":   func() runtime.Runner { return &Source{} },
+	"String":   func() runtime.Runner { return &String{} },
+	"Var":      func() runtime.Runner { return &Var{} },
 }
