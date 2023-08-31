f42385a216dd23b71ed9d20b13aad176ca280581
wenooij <142677213+wenooij@users.noreply.github.com>
2023-08-30 23:17:23 -0400

    Nuggit: Add Plan and Exchange types

--- plan.go
+++ plan.go
@@ -1,12 +1,15 @@
 package nuggit
 
+// Plan represents a multistage execution between a number of graphs.
+// Graphs are executed in concurrent stages with data exchanged
+// in accordance with the Exchange edges.
 type Plan struct {
-	Stages    []Graph    `json:"stages,omitempty"`
+	Graphs    []Graph    `json:"graphs,omitempty"`
 	Exchanges []Exchange `json:"exchanges,omitempty"`
 }
 
-// Exchange is an Edge that crosses Stage boundaries.
-// It adds the SrcGraph
+// Exchange is an Edge that crosses Graph boundaries.
+// It adds the SrcStage and DstStage fields.
 type Exchange struct {
 	SrcStage StageKey `json:"src_stage,omitempty"`
 	// DstGraph
