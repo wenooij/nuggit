4614270d52ad2dc2a06b413d8a4c20c939d9e462
Wes de Nooij <142677213+wenooij@users.noreply.github.com>
2024-07-07 14:40:15 +0000

    Ops: Implement Concat

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/wenooij/nuggit
 
 go 1.22.3
 
-require github.com/wenooij/wire v0.0.0-20240707013224-96fd61e83bf2 // indirect
+require github.com/wenooij/wire v0.0.0-20240707135948-b63f771f9b4e
--- ops/concat.go
+++ ops/concat.go
@@ -1,1 +1,39 @@
 package ops
+
+import (
+	"strings"
+
+	"github.com/wenooij/wire"
+)
+
+var concatProto = wire.Span(wire.Seq(wire.Fields(map[uint64]wire.Proto[any]{
+	1: wire.Any(wire.Seq(wire.String)),
+	2: wire.Any(wire.String),
+})))
+
+// Concat takes elements of strings and concantenates them using an optional seperator.
+func Concat(r wire.Reader) (string, error) {
+	var (
+		sb    strings.Builder
+		first bool
+		sep   string
+	)
+	msg, err := concatProto.Read(r)
+	if err != nil {
+		return "", nil
+	}
+	for _, e := range msg.Elem() {
+		switch e.Num() {
+		case 1:
+			for _, s := range e.Val().([]string) {
+				if !first {
+					sb.WriteString(sep)
+				}
+				sb.WriteString(s)
+			}
+		case 2:
+			sep = e.Val().(string)
+		}
+	}
+	return sb.String(), nil
+}
